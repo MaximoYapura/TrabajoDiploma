c5bbeabbe26483c29a9ddb45efba46b11487545f
MaximoYapura <Maximo.Yapura@alumnos.uai.edu.ar>
2026-09-15 21:04:31 -0300

    Fix RF1 790MY - parte 4b/4: FormClientes_790MY.Designer.cs (labels, textos reales, grilla solo lectura)

--- GUI/RF1/FormClientes_790MY.Designer.cs
+++ GUI/RF1/FormClientes_790MY.Designer.cs
@@ -36,12 +36,20 @@
             this.txtTelefono_790MY = new System.Windows.Forms.TextBox();
             this.btnLimpiar_790MY = new System.Windows.Forms.Button();
             this.btnRegistrar_790MY = new System.Windows.Forms.Button();
+            this.lblDni_790MY = new System.Windows.Forms.Label();
+            this.lblNombre_790MY = new System.Windows.Forms.Label();
+            this.lblApellido_790MY = new System.Windows.Forms.Label();
+            this.lblEmail_790MY = new System.Windows.Forms.Label();
+            this.lblTelefono_790MY = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvClientes_790MY)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvClientes_790MY
             // 
             this.dgvClientes_790MY.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvClientes_790MY.ReadOnly = true;
+            this.dgvClientes_790MY.AllowUserToAddRows = false;
+            this.dgvClientes_790MY.AllowUserToDeleteRows = false;
             this.dgvClientes_790MY.Location = new System.Drawing.Point(371, 107);
             this.dgvClientes_790MY.Name = "dgvClientes_790MY";
             this.dgvClientes_790MY.Size = new System.Drawing.Size(240, 150);
@@ -88,7 +96,7 @@
             this.btnLimpiar_790MY.Name = "btnLimpiar_790MY";
             this.btnLimpiar_790MY.Size = new System.Drawing.Size(91, 26);
             this.btnLimpiar_790MY.TabIndex = 6;
-            this.btnLimpiar_790MY.Text = "button1";
+            this.btnLimpiar_790MY.Text = "Limpiar";
             this.btnLimpiar_790MY.UseVisualStyleBackColor = true;
             this.btnLimpiar_790MY.Click += new System.EventHandler(this.btnLimpiar_790MY_Click);
             // 
@@ -98,15 +106,65 @@
             this.btnRegistrar_790MY.Name = "btnRegistrar_790MY";
             this.btnRegistrar_790MY.Size = new System.Drawing.Size(91, 26);
             this.btnRegistrar_790MY.TabIndex = 7;
-            this.btnRegistrar_790MY.Text = "button1";
+            this.btnRegistrar_790MY.Text = "Registrar";
             this.btnRegistrar_790MY.UseVisualStyleBackColor = true;
             this.btnRegistrar_790MY.Click += new System.EventHandler(this.btnRegistrar_790MY_Click);
+            // 
+            // lblDni_790MY
+            // 
+            this.lblDni_790MY.AutoSize = true;
+            this.lblDni_790MY.Location = new System.Drawing.Point(30, 98);
+            this.lblDni_790MY.Name = "lblDni_790MY";
+            this.lblDni_790MY.Size = new System.Drawing.Size(29, 13);
+            this.lblDni_790MY.TabIndex = 8;
+            this.lblDni_790MY.Text = "DNI:";
+            // 
+            // lblNombre_790MY
+            // 
+            this.lblNombre_790MY.AutoSize = true;
+            this.lblNombre_790MY.Location = new System.Drawing.Point(30, 154);
+            this.lblNombre_790MY.Name = "lblNombre_790MY";
+            this.lblNombre_790MY.Size = new System.Drawing.Size(46, 13);
+            this.lblNombre_790MY.TabIndex = 9;
+            this.lblNombre_790MY.Text = "Nombre:";
+            // 
+            // lblApellido_790MY
+            // 
+            this.lblApellido_790MY.AutoSize = true;
+            this.lblApellido_790MY.Location = new System.Drawing.Point(30, 212);
+            this.lblApellido_790MY.Name = "lblApellido_790MY";
+            this.lblApellido_790MY.Size = new System.Drawing.Size(48, 13);
+            this.lblApellido_790MY.TabIndex = 10;
+            this.lblApellido_790MY.Text = "Apellido:";
+            // 
+            // lblEmail_790MY
+            // 
+            this.lblEmail_790MY.AutoSize = true;
+            this.lblEmail_790MY.Location = new System.Drawing.Point(30, 275);
+            this.lblEmail_790MY.Name = "lblEmail_790MY";
+            this.lblEmail_790MY.Size = new System.Drawing.Size(37, 13);
+            this.lblEmail_790MY.TabIndex = 11;
+            this.lblEmail_790MY.Text = "Email:";
+            // 
+            // lblTelefono_790MY
+            // 
+            this.lblTelefono_790MY.AutoSize = true;
+            this.lblTelefono_790MY.Location = new System.Drawing.Point(30, 333);
+            this.lblTelefono_790MY.Name = "lblTelefono_790MY";
+            this.lblTelefono_790MY.Size = new System.Drawing.Size(53, 13);
+            this.lblTelefono_790MY.TabIndex = 12;
+            this.lblTelefono_790MY.Text = "Teléfono:";
             // 
             // FormClientes_790MY
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTelefono_790MY);
+            this.Controls.Add(this.lblEmail_790MY);
+            this.Controls.Add(this.lblApellido_790MY);
+            this.Controls.Add(this.lblNombre_790MY);
+            this.Controls.Add(this.lblDni_790MY);
             this.Controls.Add(this.btnRegistrar_790MY);
             this.Controls.Add(this.btnLimpiar_790MY);
             this.Controls.Add(this.txtTelefono_790MY);
@@ -134,5 +192,10 @@
         private System.Windows.Forms.TextBox txtTelefono_790MY;
         private System.Windows.Forms.Button btnLimpiar_790MY;
         private System.Windows.Forms.Button btnRegistrar_790MY;
+        private System.Windows.Forms.Label lblDni_790MY;
+        private System.Windows.Forms.Label lblNombre_790MY;
+        private System.Windows.Forms.Label lblApellido_790MY;
+        private System.Windows.Forms.Label lblEmail_790MY;
+        private System.Windows.Forms.Label lblTelefono_790MY;
     }
 }
