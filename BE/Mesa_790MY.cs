1a474ff448ea8e7651074a21d4222ea293e45237
MaximoYapura <Maximo.Yapura@alumnos.uai.edu.ar>
2026-09-16 17:52:36 -0300

    RF Seleccionar Mesa 790MY - parte 1/4: entidades Mesa_790MY y esqueleto Reserva_790MY

---
+++ BE/Mesa_790MY.cs
@@ -0,0 +1,52 @@
+﻿namespace BE_08YS
+{
+    public enum EstadoMesa_790MY
+    {
+        Libre,
+        Ocupada,
+        Reservada
+    }
+
+    public class Mesa_790MY
+    {
+        private int _nroMesa;
+        private int _capacidad;
+        private EstadoMesa_790MY _estado;
+
+        public Mesa_790MY(int nroMesa, int capacidad, EstadoMesa_790MY estado)
+        {
+            _nroMesa = nroMesa;
+            _capacidad = capacidad;
+            _estado = estado;
+        }
+
+        public Mesa_790MY() { }
+
+        public int NroMesa
+        {
+            get { return _nroMesa; }
+            set { _nroMesa = value; }
+        }
+
+        public int Capacidad
+        {
+            get { return _capacidad; }
+            set { _capacidad = value; }
+        }
+
+        public EstadoMesa_790MY Estado
+        {
+            get { return _estado; }
+            set { _estado = value; }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Mesa_790MY other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return NroMesa == other.NroMesa;
+        }
+
+        public override int GetHashCode() => NroMesa.GetHashCode();
+    }
+}
---
+++ BE/Reserva_790MY.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace BE_08YS
+{
+    public enum EstadoReserva_790MY
+    {
+        Confirmada,
+        Cancelada
+    }
+
+    // Esqueleto minimo: solo lo indispensable para que SqlMesaRepository_790MY.GetDisponibles
+    // pueda hacer el NOT EXISTS contra reservas activas. El CU "Generar Reserva" completo
+    // (con sus propias validaciones e integracion con Cliente) queda para una etapa posterior.
+    public class Reserva_790MY
+    {
+        private int _reservaId;
+        private int _clienteDni;
+        private int _mesaNumero;
+        private DateTime _fecha;
+        private TimeSpan _hora;
+        private int _cantidadComensales;
+        private EstadoReserva_790MY _estado;
+
+        public Reserva_790MY(int clienteDni, int mesaNumero, DateTime fecha, TimeSpan hora, int cantidadComensales, EstadoReserva_790MY estado)
+        {
+            _clienteDni = clienteDni;
+            _mesaNumero = mesaNumero;
+            _fecha = fecha;
+            _hora = hora;
+            _cantidadComensales = cantidadComensales;
+            _estado = estado;
+        }
+
+        public Reserva_790MY() { }
+
+        public int ReservaID
+        {
+            get { return _reservaId; }
+            set { _reservaId = value; }
+        }
+
+        public int ClienteDNI
+        {
+            get { return _clienteDni; }
+            set { _clienteDni = value; }
+        }
+
+        public int MesaNumero
+        {
+            get { return _mesaNumero; }
+            set { _mesaNumero = value; }
+        }
+
+        public DateTime Fecha
+        {
+            get { return _fecha; }
+            set { _fecha = value; }
+        }
+
+        public TimeSpan Hora
+        {
+            get { return _hora; }
+            set { _hora = value; }
+        }
+
+        public int CantidadComensales
+        {
+            get { return _cantidadComensales; }
+            set { _cantidadComensales = value; }
+        }
+
+        public EstadoReserva_790MY Estado
+        {
+            get { return _estado; }
+            set { _estado = value; }
+        }
+    }
+}
