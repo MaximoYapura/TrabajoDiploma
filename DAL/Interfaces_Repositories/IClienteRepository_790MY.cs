c828433441c113e908438fbad8c2dda5bd532ba7
MaximoYapura <Maximo.Yapura@alumnos.uai.edu.ar>
2026-09-17 18:18:38 -0300

    ABM-C Clientes 790MY - 4a/7: IClienteRepository_790MY con Update/DeleteLogico

--- DAL/Interfaces_Repositories/IClienteRepository_790MY.cs
+++ DAL/Interfaces_Repositories/IClienteRepository_790MY.cs
@@ -8,8 +8,12 @@ namespace DAL_08YS.Interfaces_Repositories
 {
     public interface IClienteRepository_790MY
     {
+        // Todos los metodos de consulta (Exists, GetByDni, GetAll) operan solo
+        // sobre clientes con Activo = 1 (borrado logico).
         bool Exists(int dni);
         void Create(Cliente_790MY cliente);
+        void Update(Cliente_790MY cliente);
+        void DeleteLogico(int dni);
         List<Cliente_790MY> GetAll();
         Cliente_790MY GetByDni(int dni);
     }
