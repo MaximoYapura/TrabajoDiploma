2af3a8685a6487876444e412ddac8dc04b0f04ab
MaximoYapura <Maximo.Yapura@alumnos.uai.edu.ar>
2026-09-16 23:47:09 -0300

    RF Seleccionar Mesa 790MY - GUI 2/2: FormSeleccionarMesa_790MY (modal) + Designer

---
+++ GUI/RF1/FormSeleccionarMesa_790MY.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace GUI_08YS.RF1
+{
+    partial class FormSeleccionarMesa_790MY
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.lblTitulo_790MY = new System.Windows.Forms.Label();
+            this.lblSinResultados_790MY = new System.Windows.Forms.Label();
+            this.flowLayoutMesas_790MY = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnAceptar_790MY = new System.Windows.Forms.Button();
+            this.btnCancelar_790MY = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            //
+            // lblTitulo_790MY
+            //
+            this.lblTitulo_790MY.AutoSize = true;
+            this.lblTitulo_790MY.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.lblTitulo_790MY.Location = new System.Drawing.Point(20, 15);
+            this.lblTitulo_790MY.Name = "lblTitulo_790MY";
+            this.lblTitulo_790MY.Size = new System.Drawing.Size(160, 25);
+            this.lblTitulo_790MY.Text = "Seleccionar Mesa";
+            //
+            // lblSinResultados_790MY
+            //
+            this.lblSinResultados_790MY.AutoSize = true;
+            this.lblSinResultados_790MY.ForeColor = System.Drawing.Color.Firebrick;
+            this.lblSinResultados_790MY.Location = new System.Drawing.Point(20, 60);
+            this.lblSinResultados_790MY.Name = "lblSinResultados_790MY";
+            this.lblSinResultados_790MY.Size = new System.Drawing.Size(280, 15);
+            this.lblSinResultados_790MY.Text = "No hay mesas disponibles para ese turno.";
+            this.lblSinResultados_790MY.Visible = false;
+            //
+            // flowLayoutMesas_790MY
+            //
+            this.flowLayoutMesas_790MY.AutoScroll = true;
+            this.flowLayoutMesas_790MY.Location = new System.Drawing.Point(20, 90);
+            this.flowLayoutMesas_790MY.Name = "flowLayoutMesas_790MY";
+            this.flowLayoutMesas_790MY.Size = new System.Drawing.Size(560, 320);
+            this.flowLayoutMesas_790MY.TabIndex = 0;
+            //
+            // btnAceptar_790MY
+            //
+            this.btnAceptar_790MY.Enabled = false;
+            this.btnAceptar_790MY.Location = new System.Drawing.Point(390, 430);
+            this.btnAceptar_790MY.Name = "btnAceptar_790MY";
+            this.btnAceptar_790MY.Size = new System.Drawing.Size(90, 30);
+            this.btnAceptar_790MY.TabIndex = 1;
+            this.btnAceptar_790MY.Text = "Aceptar";
+            this.btnAceptar_790MY.UseVisualStyleBackColor = true;
+            this.btnAceptar_790MY.Click += new System.EventHandler(this.btnAceptar_790MY_Click);
+            //
+            // btnCancelar_790MY
+            //
+            this.btnCancelar_790MY.Location = new System.Drawing.Point(490, 430);
+            this.btnCancelar_790MY.Name = "btnCancelar_790MY";
+            this.btnCancelar_790MY.Size = new System.Drawing.Size(90, 30);
+            this.btnCancelar_790MY.TabIndex = 2;
+            this.btnCancelar_790MY.Text = "Cancelar";
+            this.btnCancelar_790MY.UseVisualStyleBackColor = true;
+            this.btnCancelar_790MY.Click += new System.EventHandler(this.btnCancelar_790MY_Click);
+            //
+            // FormSeleccionarMesa_790MY
+            //
+            this.AcceptButton = this.btnAceptar_790MY;
+            this.CancelButton = this.btnCancelar_790MY;
+            this.ClientSize = new System.Drawing.Size(600, 480);
+            this.Controls.Add(this.btnCancelar_790MY);
+            this.Controls.Add(this.btnAceptar_790MY);
+            this.Controls.Add(this.flowLayoutMesas_790MY);
+            this.Controls.Add(this.lblSinResultados_790MY);
+            this.Controls.Add(this.lblTitulo_790MY);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormSeleccionarMesa_790MY";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Seleccionar Mesa";
+            this.Load += new System.EventHandler(this.FormSeleccionarMesa_790MY_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo_790MY;
+        private System.Windows.Forms.Label lblSinResultados_790MY;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutMesas_790MY;
+        private System.Windows.Forms.Button btnAceptar_790MY;
+        private System.Windows.Forms.Button btnCancelar_790MY;
+    }
+}
---
+++ GUI/RF1/FormSeleccionarMesa_790MY.cs
@@ -0,0 +1,110 @@
+﻿using BE_08YS;
+using BLL_08YS;
+using CustomControls;
+using System;
+using System.Windows.Forms;
+
+namespace GUI_08YS.RF1
+{
+    public partial class FormSeleccionarMesa_790MY : Form
+    {
+        private readonly MesaBLL_790MY _mesaBll;
+        private readonly DateTime _fecha;
+        private readonly TimeSpan _hora;
+        private readonly int _comensales;
+
+        private UserControlMesa_790MY _tarjetaSeleccionada;
+
+        public Mesa_790MY MesaSeleccionada { get; private set; }
+
+        public FormSeleccionarMesa_790MY(DateTime fecha, TimeSpan hora, int comensales)
+        {
+            InitializeComponent();
+
+            _fecha = fecha;
+            _hora = hora;
+            _comensales = comensales;
+            _mesaBll = BLLFactory_790MY.CreateMesaBLL();
+        }
+
+        private void FormSeleccionarMesa_790MY_Load(object sender, EventArgs e)
+        {
+            CargarMesasDisponibles();
+        }
+
+        private void CargarMesasDisponibles()
+        {
+            flowLayoutMesas_790MY.Controls.Clear();
+            _tarjetaSeleccionada = null;
+            MesaSeleccionada = null;
+            btnAceptar_790MY.Enabled = false;
+
+            try
+            {
+                var disponibles = _mesaBll.BuscarDisponibles(_fecha, _hora, _comensales);
+
+                lblSinResultados_790MY.Visible = disponibles.Count == 0;
+
+                foreach (var mesa in disponibles)
+                {
+                    var tarjeta = new UserControlMesa_790MY
+                    {
+                        Mesa = mesa,
+                        EstadoVisual = EstadoMesa_790MY.Libre, // todo lo que llega acá ya está disponible para este turno
+                        Seleccionable = true,
+                        Margin = new Padding(8)
+                    };
+                    tarjeta.MesaClick += Tarjeta_MesaClick;
+                    flowLayoutMesas_790MY.Controls.Add(tarjeta);
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message, "Datos inválidos", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void Tarjeta_MesaClick(object sender, EventArgs e)
+        {
+            var tarjeta = (UserControlMesa_790MY)sender;
+
+            if (_tarjetaSeleccionada != null)
+                _tarjetaSeleccionada.Seleccionada = false;
+
+            if (_tarjetaSeleccionada == tarjeta)
+            {
+                // click sobre la misma tarjeta ya seleccionada -> deseleccionar
+                _tarjetaSeleccionada = null;
+                MesaSeleccionada = null;
+            }
+            else
+            {
+                tarjeta.Seleccionada = true;
+                _tarjetaSeleccionada = tarjeta;
+                MesaSeleccionada = tarjeta.Mesa;
+            }
+
+            btnAceptar_790MY.Enabled = MesaSeleccionada != null;
+        }
+
+        private void btnAceptar_790MY_Click(object sender, EventArgs e)
+        {
+            if (MesaSeleccionada == null)
+            {
+                MessageBox.Show("Seleccioná una mesa antes de continuar.", "Falta seleccionar",
+                                 MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void btnCancelar_790MY_Click(object sender, EventArgs e)
+        {
+            MesaSeleccionada = null;
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+    }
+}
