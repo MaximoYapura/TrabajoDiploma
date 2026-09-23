bc787ed61b27f70c2a0a5b551ce0a3898f028560
MaximoYapura <Maximo.Yapura@alumnos.uai.edu.ar>
2026-09-23 13:48:26 -0300

    Maestros carpeta

---
+++ GUI/Maestros/FormMesas_790MY.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace GUI_08YS.RF1
+{
+    partial class FormMesas_790MY
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.dgvMesas_790MY = new System.Windows.Forms.DataGridView();
+            this.colNumero_790MY = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colCapacidad_790MY = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colEstado_790MY = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnAnadir_790MY = new System.Windows.Forms.Button();
+            this.btnModificar_790MY = new System.Windows.Forms.Button();
+            this.btnEliminar_790MY = new System.Windows.Forms.Button();
+            this.btnAplicar_790MY = new System.Windows.Forms.Button();
+            this.btnCancelar_790MY = new System.Windows.Forms.Button();
+            this.btnSalir_790MY = new System.Windows.Forms.Button();
+            this.lblNumero_790MY = new System.Windows.Forms.Label();
+            this.txtNumero_790MY = new System.Windows.Forms.TextBox();
+            this.lblCapacidad_790MY = new System.Windows.Forms.Label();
+            this.txtCapacidad_790MY = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMesas_790MY)).BeginInit();
+            this.SuspendLayout();
+            //
+            // dgvMesas_790MY
+            //
+            this.dgvMesas_790MY.AllowUserToAddRows = false;
+            this.dgvMesas_790MY.AllowUserToDeleteRows = false;
+            this.dgvMesas_790MY.ReadOnly = true;
+            this.dgvMesas_790MY.RowHeadersVisible = false;
+            this.dgvMesas_790MY.AutoGenerateColumns = false;
+            this.dgvMesas_790MY.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvMesas_790MY.MultiSelect = false;
+            this.dgvMesas_790MY.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(248)))), ((int)(((byte)(245)))));
+            this.dgvMesas_790MY.EnableHeadersVisualStyles = false;
+            this.dgvMesas_790MY.ColumnHeadersDefaultCellStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.dgvMesas_790MY.ColumnHeadersDefaultCellStyle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(195)))), ((int)(((byte)(140)))));
+            this.dgvMesas_790MY.DefaultCellStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(248)))), ((int)(((byte)(245)))));
+            this.dgvMesas_790MY.DefaultCellStyle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(42)))), ((int)(((byte)(40)))));
+            this.dgvMesas_790MY.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colNumero_790MY,
+            this.colCapacidad_790MY,
+            this.colEstado_790MY});
+            this.dgvMesas_790MY.Location = new System.Drawing.Point(20, 20);
+            this.dgvMesas_790MY.Name = "dgvMesas_790MY";
+            this.dgvMesas_790MY.Size = new System.Drawing.Size(450, 300);
+            this.dgvMesas_790MY.SelectionChanged += new System.EventHandler(this.dgvMesas_790MY_SelectionChanged);
+            //
+            // colNumero_790MY
+            //
+            this.colNumero_790MY.DataPropertyName = "NroMesa";
+            this.colNumero_790MY.HeaderText = "Número";
+            this.colNumero_790MY.Name = "colNumero_790MY";
+            this.colNumero_790MY.ReadOnly = true;
+            //
+            // colCapacidad_790MY
+            //
+            this.colCapacidad_790MY.DataPropertyName = "Capacidad";
+            this.colCapacidad_790MY.HeaderText = "Capacidad";
+            this.colCapacidad_790MY.Name = "colCapacidad_790MY";
+            this.colCapacidad_790MY.ReadOnly = true;
+            //
+            // colEstado_790MY
+            //
+            this.colEstado_790MY.DataPropertyName = "Estado";
+            this.colEstado_790MY.HeaderText = "Estado";
+            this.colEstado_790MY.Name = "colEstado_790MY";
+            this.colEstado_790MY.ReadOnly = true;
+            //
+            // Botones de acción (lado derecho de la grilla)
+            //
+            this.btnAnadir_790MY.Location = new System.Drawing.Point(485, 20);
+            this.btnAnadir_790MY.Name = "btnAnadir_790MY";
+            this.btnAnadir_790MY.Size = new System.Drawing.Size(110, 28);
+            this.btnAnadir_790MY.Text = "Añadir";
+            this.btnAnadir_790MY.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAnadir_790MY.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.btnAnadir_790MY.ForeColor = System.Drawing.Color.White;
+            this.btnAnadir_790MY.UseVisualStyleBackColor = false;
+            this.btnAnadir_790MY.Click += new System.EventHandler(this.btnAnadir_790MY_Click);
+            //
+            this.btnModificar_790MY.Location = new System.Drawing.Point(485, 55);
+            this.btnModificar_790MY.Name = "btnModificar_790MY";
+            this.btnModificar_790MY.Size = new System.Drawing.Size(110, 28);
+            this.btnModificar_790MY.Text = "Modificar";
+            this.btnModificar_790MY.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnModificar_790MY.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.btnModificar_790MY.ForeColor = System.Drawing.Color.White;
+            this.btnModificar_790MY.UseVisualStyleBackColor = false;
+            this.btnModificar_790MY.Click += new System.EventHandler(this.btnModificar_790MY_Click);
+            //
+            this.btnEliminar_790MY.Location = new System.Drawing.Point(485, 90);
+            this.btnEliminar_790MY.Name = "btnEliminar_790MY";
+            this.btnEliminar_790MY.Size = new System.Drawing.Size(110, 28);
+            this.btnEliminar_790MY.Text = "Eliminar";
+            this.btnEliminar_790MY.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEliminar_790MY.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.btnEliminar_790MY.ForeColor = System.Drawing.Color.White;
+            this.btnEliminar_790MY.UseVisualStyleBackColor = false;
+            this.btnEliminar_790MY.Click += new System.EventHandler(this.btnEliminar_790MY_Click);
+            //
+            this.btnAplicar_790MY.Location = new System.Drawing.Point(485, 140);
+            this.btnAplicar_790MY.Name = "btnAplicar_790MY";
+            this.btnAplicar_790MY.Size = new System.Drawing.Size(110, 28);
+            this.btnAplicar_790MY.Text = "Aplicar";
+            this.btnAplicar_790MY.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAplicar_790MY.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.btnAplicar_790MY.ForeColor = System.Drawing.Color.White;
+            this.btnAplicar_790MY.UseVisualStyleBackColor = false;
+            this.btnAplicar_790MY.Click += new System.EventHandler(this.btnAplicar_790MY_Click);
+            //
+            this.btnCancelar_790MY.Location = new System.Drawing.Point(485, 175);
+            this.btnCancelar_790MY.Name = "btnCancelar_790MY";
+            this.btnCancelar_790MY.Size = new System.Drawing.Size(110, 28);
+            this.btnCancelar_790MY.Text = "Cancelar";
+            this.btnCancelar_790MY.UseVisualStyleBackColor = true;
+            this.btnCancelar_790MY.Click += new System.EventHandler(this.btnCancelar_790MY_Click);
+            //
+            this.btnSalir_790MY.Location = new System.Drawing.Point(485, 212);
+            this.btnSalir_790MY.Name = "btnSalir_790MY";
+            this.btnSalir_790MY.Size = new System.Drawing.Size(110, 28);
+            this.btnSalir_790MY.Text = "Salir";
+            this.btnSalir_790MY.UseVisualStyleBackColor = true;
+            this.btnSalir_790MY.Click += new System.EventHandler(this.btnSalir_790MY_Click);
+            //
+            // Formulario inferior de edición
+            //
+            this.lblNumero_790MY.AutoSize = true;
+            this.lblNumero_790MY.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(42)))), ((int)(((byte)(40)))));
+            this.lblNumero_790MY.Location = new System.Drawing.Point(20, 340);
+            this.lblNumero_790MY.Name = "lblNumero_790MY";
+            this.lblNumero_790MY.Text = "Número:";
+            //
+            this.txtNumero_790MY.Location = new System.Drawing.Point(110, 337);
+            this.txtNumero_790MY.Name = "txtNumero_790MY";
+            this.txtNumero_790MY.Size = new System.Drawing.Size(120, 20);
+            //
+            this.lblCapacidad_790MY.AutoSize = true;
+            this.lblCapacidad_790MY.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(42)))), ((int)(((byte)(40)))));
+            this.lblCapacidad_790MY.Location = new System.Drawing.Point(20, 372);
+            this.lblCapacidad_790MY.Name = "lblCapacidad_790MY";
+            this.lblCapacidad_790MY.Text = "Capacidad:";
+            //
+            this.txtCapacidad_790MY.Location = new System.Drawing.Point(110, 369);
+            this.txtCapacidad_790MY.Name = "txtCapacidad_790MY";
+            this.txtCapacidad_790MY.Size = new System.Drawing.Size(120, 20);
+            //
+            // FormMesas_790MY
+            //
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(248)))), ((int)(((byte)(245)))));
+            this.ClientSize = new System.Drawing.Size(630, 420);
+            this.Controls.Add(this.dgvMesas_790MY);
+            this.Controls.Add(this.btnAnadir_790MY);
+            this.Controls.Add(this.btnModificar_790MY);
+            this.Controls.Add(this.btnEliminar_790MY);
+            this.Controls.Add(this.btnAplicar_790MY);
+            this.Controls.Add(this.btnCancelar_790MY);
+            this.Controls.Add(this.btnSalir_790MY);
+            this.Controls.Add(this.lblNumero_790MY);
+            this.Controls.Add(this.txtNumero_790MY);
+            this.Controls.Add(this.lblCapacidad_790MY);
+            this.Controls.Add(this.txtCapacidad_790MY);
+            this.Name = "FormMesas_790MY";
+            this.Text = "Mesas";
+            this.Load += new System.EventHandler(this.FormMesas_790MY_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMesas_790MY)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvMesas_790MY;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colNumero_790MY;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colCapacidad_790MY;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colEstado_790MY;
+        private System.Windows.Forms.Button btnAnadir_790MY;
+        private System.Windows.Forms.Button btnModificar_790MY;
+        private System.Windows.Forms.Button btnEliminar_790MY;
+        private System.Windows.Forms.Button btnAplicar_790MY;
+        private System.Windows.Forms.Button btnCancelar_790MY;
+        private System.Windows.Forms.Button btnSalir_790MY;
+        private System.Windows.Forms.Label lblNumero_790MY;
+        private System.Windows.Forms.TextBox txtNumero_790MY;
+        private System.Windows.Forms.Label lblCapacidad_790MY;
+        private System.Windows.Forms.TextBox txtCapacidad_790MY;
+    }
+}
---
+++ GUI/Maestros/FormMesas_790MY.cs
@@ -0,0 +1,196 @@
+﻿using BE_08YS;
+using BLL_08YS;
+using Service_08YS.Entities.Acceso;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace GUI_08YS.RF1
+{
+    public partial class FormMesas_790MY : Form
+    {
+        private enum ModoEdicion { Reposo, Alta, Modificacion }
+
+        private static readonly Dictionary<string, Permisos> _mapaPermisos =
+            new Dictionary<string, Permisos>
+            {
+                { nameof(btnAnadir_790MY), Permisos.CrearMesa },
+                { nameof(btnModificar_790MY), Permisos.CrearMesa },
+                { nameof(btnEliminar_790MY), Permisos.CrearMesa }
+            };
+
+        private readonly MesaBLL_790MY _mesaBll;
+        private ModoEdicion _modo = ModoEdicion.Reposo;
+
+        // Estado de la mesa que se esta editando: no es un campo del formulario
+        // (solo se editan Número y Capacidad), pero hay que preservarlo al modificar
+        // para no pisarlo con un valor por defecto.
+        private EstadoMesa_790MY _estadoEdicion = EstadoMesa_790MY.Libre;
+
+        public FormMesas_790MY()
+        {
+            InitializeComponent();
+            _mesaBll = BLLFactory_790MY.CreateMesaBLL();
+        }
+
+        private void FormMesas_790MY_Load(object sender, EventArgs e)
+        {
+            PermissionFilter_08YS.Aplicar(this, _mapaPermisos);
+            CargarGrilla();
+            EstablecerModo(ModoEdicion.Reposo);
+        }
+
+        private void CargarGrilla()
+        {
+            dgvMesas_790MY.DataSource = null;
+            dgvMesas_790MY.DataSource = _mesaBll.GetAll();
+        }
+
+        private void dgvMesas_790MY_SelectionChanged(object sender, EventArgs e)
+        {
+            if (_modo == ModoEdicion.Reposo)
+                ActualizarHabilitacionBotonesAccion();
+        }
+
+        private void ActualizarHabilitacionBotonesAccion()
+        {
+            bool haySeleccion = dgvMesas_790MY.CurrentRow?.DataBoundItem is Mesa_790MY;
+            btnModificar_790MY.Enabled = haySeleccion;
+            btnEliminar_790MY.Enabled = haySeleccion;
+        }
+
+        private void EstablecerModo(ModoEdicion modo)
+        {
+            _modo = modo;
+            bool editando = modo != ModoEdicion.Reposo;
+
+            txtNumero_790MY.Enabled = modo == ModoEdicion.Alta;
+            txtCapacidad_790MY.Enabled = editando;
+
+            btnAplicar_790MY.Enabled = editando;
+            btnCancelar_790MY.Enabled = editando;
+
+            dgvMesas_790MY.Enabled = !editando;
+            btnAnadir_790MY.Enabled = !editando;
+
+            if (!editando)
+                ActualizarHabilitacionBotonesAccion();
+            else
+                btnModificar_790MY.Enabled = btnEliminar_790MY.Enabled = false;
+        }
+
+        private void LimpiarCampos()
+        {
+            txtNumero_790MY.Clear();
+            txtCapacidad_790MY.Clear();
+            _estadoEdicion = EstadoMesa_790MY.Libre;
+        }
+
+        private void btnAnadir_790MY_Click(object sender, EventArgs e)
+        {
+            LimpiarCampos();
+            EstablecerModo(ModoEdicion.Alta);
+            txtNumero_790MY.Focus();
+        }
+
+        private void btnModificar_790MY_Click(object sender, EventArgs e)
+        {
+            if (!(dgvMesas_790MY.CurrentRow?.DataBoundItem is Mesa_790MY seleccionada))
+                return;
+
+            txtNumero_790MY.Text = seleccionada.NroMesa.ToString();
+            txtCapacidad_790MY.Text = seleccionada.Capacidad.ToString();
+            _estadoEdicion = seleccionada.Estado;
+
+            EstablecerModo(ModoEdicion.Modificacion);
+        }
+
+        private void btnEliminar_790MY_Click(object sender, EventArgs e)
+        {
+            if (!(dgvMesas_790MY.CurrentRow?.DataBoundItem is Mesa_790MY seleccionada))
+                return;
+
+            var respuesta = MessageBox.Show(
+                $"¿Confirma dar de baja la mesa Nro. {seleccionada.NroMesa}? " +
+                "Esto no afecta las reservas ya registradas para esa mesa.",
+                "Confirmar baja", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (respuesta != DialogResult.Yes) return;
+
+            try
+            {
+                _mesaBll.EliminarMesa(seleccionada.NroMesa);
+                MessageBox.Show("Mesa dada de baja correctamente.", "Éxito",
+                                 MessageBoxButtons.OK, MessageBoxIcon.Information);
+                CargarGrilla();
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message, "No se pudo eliminar", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ocurrió un error inesperado al eliminar la mesa.", "Error",
+                                 MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnAplicar_790MY_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(txtNumero_790MY.Text.Trim(), out int numero))
+            {
+                MessageBox.Show("El número de mesa debe ser un valor numérico.", "Dato inválido",
+                                 MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (!int.TryParse(txtCapacidad_790MY.Text.Trim(), out int capacidad))
+            {
+                MessageBox.Show("La capacidad debe ser un valor numérico.", "Dato inválido",
+                                 MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                if (_modo == ModoEdicion.Alta)
+                {
+                    _mesaBll.RegistrarMesa(numero, capacidad);
+                    MessageBox.Show("Mesa registrada correctamente.", "Éxito",
+                                     MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else if (_modo == ModoEdicion.Modificacion)
+                {
+                    var mesa = new Mesa_790MY(numero, capacidad, _estadoEdicion);
+                    _mesaBll.ModificarMesa(mesa);
+                    MessageBox.Show("Mesa modificada correctamente.", "Éxito",
+                                     MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+                LimpiarCampos();
+                CargarGrilla();
+                EstablecerModo(ModoEdicion.Reposo);
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message, "Datos inválidos", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ocurrió un error inesperado al guardar la mesa.", "Error",
+                                 MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnCancelar_790MY_Click(object sender, EventArgs e)
+        {
+            LimpiarCampos();
+            EstablecerModo(ModoEdicion.Reposo);
+        }
+
+        private void btnSalir_790MY_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
